38730c97f80ec6c8d79a34880e3418ebde7c2063
Thomas Weinert <weinert@dimensional.de>
2014-05-02 15:21:43 +0200

    # Initial Commit, separate module from cms/framework repository

---
+++ src/_classmap.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'cronjob_todos_reminder' => '/cronjob_todos_reminder.php'
+  'time_crontab' => '/time_crontab.php',
+  'time_dailyhour' => '/time_dailyhour.php',
+  'time_datetime' => '/time_datetime.php',
+  'time_timeframe' => '/time_timeframe.php',
+  'time_weekdayperdays' => '/time_weekdayperdays.php',
+  'time_weekdaypermonth' => '/time_weekdaypermonth.php'
+);
