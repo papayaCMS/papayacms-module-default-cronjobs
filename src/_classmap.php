a4167d9969502367159af3587a3e132382875e65
kersken <kersken@dimensional.de>
2016-09-21 16:09:15 +0200

    Fixed a typo: added missing comma

--- src/_classmap.php
+++ src/_classmap.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-  'cronjob_todos_reminder' => '/cronjob_todos_reminder.php'
+  'cronjob_todos_reminder' => '/cronjob_todos_reminder.php',
   'time_crontab' => '/time_crontab.php',
   'time_dailyhour' => '/time_dailyhour.php',
   'time_datetime' => '/time_datetime.php',
